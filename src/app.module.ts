ed2a81e7e0bcea5e768723c4d425f3f727aa3572
QFo <qiufo9527@163.com>
2023-11-30 07:51:52 +0800

    学习第 20 - 24 节课：学习到使用 docker 容器化生成 postgres 数据库 & 学习使用 TypeORM 在 Nest 配合使用

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true, // 有助于自动加载模块，而不用指定实体数据
+      synchronize: true, // 确保 TypeORM 实体会同步数据库
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/coffees/entities/coffees.entity.ts
+++ src/coffees/entities/coffees.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity() // sql table === 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn() // 设置为主键并自增
   id: number;
+
+  @Column() // 啥没传时表示必填入
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true }) // TypeORM 会清楚该字段保存为 JSON，并且允许为 null
   flavors: string[];
 }
