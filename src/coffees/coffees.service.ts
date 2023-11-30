97d7818b641475eece927239f46b2ef7fead1e5e
QFo <qiufo9527@163.com>
2023-11-30 20:21:17 +0800

    feat: 学习第 26 节：
    - 学习向 coffees.service.ts 中通过 typeorm 引入真实数据库内容
    - 实现方法内的 CRUD

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -7,24 +7,26 @@ import {
 import { Coffee } from './entities/coffees.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Shipwreck Roast',
-      brand: 'Buddy Brew',
-      flavors: ['chocolate', 'vanilla'],
-    },
-  ];
+  constructor(
+    @InjectRepository(Coffee)
+    private readonly coffeeRepository: Repository<Coffee>,
+  ) {}
 
   findAll() {
-    return this.coffees;
+    return this.coffeeRepository.find();
   }
 
-  findOne(id: string) {
-    const coffee = this.coffees.find((item) => item.id === +id);
+  async findOne(id: string) {
+    const coffee = await this.coffeeRepository.findOne({
+      where: {
+        id: +id,
+      },
+    });
     if (!coffee) {
       // return new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
       return new NotFoundException(`Coffee #${id} not found`);
@@ -33,29 +35,28 @@ export class CoffeesService {
   }
 
   create(createCoffeeDto: CreateCoffeeDto) {
-    this.coffees.push({
-      id: Math.floor(Math.random() * 1000),
-      ...createCoffeeDto,
+    const coffee = this.coffeeRepository.create(createCoffeeDto);
+    return this.coffeeRepository.save(coffee);
+  }
+
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = await this.coffeeRepository.preload({
+      id: +id,
+      ...updateCoffeeDto,
     });
-    return createCoffeeDto;
-  }
-
-  update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
-    const existingCoffee = this.findOne(id);
-    if (existingCoffee) {
+    if (!existingCoffee) {
       // update the existing entity
-      for (const key in updateCoffeeDto) {
-        if (updateCoffeeDto.hasOwnProperty(key)) {
-          existingCoffee[key] = updateCoffeeDto[key];
-        }
-      }
+      return new NotFoundException(`Coffee #${id} not found`);
     }
+    return this.coffeeRepository.save(existingCoffee);
   }
 
-  remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+  async remove(id: string) {
+    const coffee = await this.coffeeRepository.findOne({
+      where: {
+        id: +id,
+      },
+    });
+    return this.coffeeRepository.remove(coffee);
   }
 }
