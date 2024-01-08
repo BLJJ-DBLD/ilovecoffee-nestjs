9454f9acb69894c8a7adf94f543ac429c0ea5dd9
QFo <qiufo9527@163.com>
2024-01-08 20:01:27 +0800

    feat: 学习实现动态 Module，方便传入参数配置

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
