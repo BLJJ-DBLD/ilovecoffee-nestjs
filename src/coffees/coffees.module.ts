2aa2a7cb6e1650ec27b1fe0913142b515612c0cc
QFo <qiufo9527@163.com>
2024-01-08 19:51:14 +0800

    feat: 学习 providers 的自定义程序：useValue、useClass、useFactory

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,15 +1,53 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffees.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    return ['buddy brew', 'nescafe', 'starbucks'];
+  }
+}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    },
+    CoffeeBrandsFactory,
+    /* {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) => {
+        return brandsFactory.create();
+      },
+      inject: [CoffeeBrandsFactory], // 还必须将 CoffeeBrandsFactory 引入 providers 中
+    }, */
+    /* 异步 useFactory */
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: async (brandsFactory: CoffeeBrandsFactory) => {
+        const brands = await Promise.resolve(brandsFactory.create());
+        return brands;
+      },
+      inject: [CoffeeBrandsFactory],
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,6 +1,7 @@
 import {
   HttpException,
   HttpStatus,
+  Inject,
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
@@ -12,6 +13,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Flavor } from './entities/flavor.entity';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Injectable()
 export class CoffeesService {
@@ -23,7 +25,11 @@ export class CoffeesService {
     private readonly flavorRepository: Repository<Flavor>,
 
     private readonly connection: Connection,
-  ) {}
+
+    @Inject(COFFEE_BRANDS) coffeeBrands: string[],
+  ) {
+    console.log(coffeeBrands, '控制台输出');
+  }
 
   findAll(paginationQuery: PaginationQueryDto) {
     const { limit, offset } = paginationQuery;
